c68a748b0c9b6f2c5f4a0c18e976ac6df2d97899
m-weitzel <m.weitzel@mpic.de>
2017-06-20 16:00:14 +0200

    Extracted Plotting routines. QoL improvements.

--- IceSizing.py
+++ IceSizing.py
@@ -6,6 +6,7 @@ from imutils import contours
 import os
 from matplotlib import pyplot as plt
 import pickle
+import sys
 
 
 class Particle:
@@ -145,6 +146,9 @@ def get_box_metrics(contour, img, pixels_per_metric):
     dim_w = w / pixels_per_metric
     area = cv2.contourArea(contour) / (pixels_per_metric ** 2)
 
+    if dim_l > 100:
+        return [], img
+
     dimensions = (dim_l, dim_w, area)
 
     # draw the object sizes on the img
@@ -208,8 +212,9 @@ def process_img_obj(img_object):
     return ls, ws, cnt_area, aspect_ratio, area_ratio
 
 
-#folder_list = ('img/ice/3103M1', 'img/ice/2804M1', 'img/ice/2804M2')
-folder_list = ('img/ice/2203M1', 'img/ice/2203M2', 'img/ice/2203M3')
+folder_list = ('img/ice/2203M1', 'img/ice/2203M2', 'img/ice/2203M3','img/ice/3103M1','img/ice/0604M1', 'img/ice/0604M2', 'img/ice/0604M1', 'img/ice/2804M1', 'img/ice/2804M2')
+#folder_list = ('img/ice/2203M1', 'img/ice/2203M2', 'img/ice/2203M3')
+#folder_list = ('img/ice/0604M1', 'img/ice/0604M2')
 
 folder_count = 1
 folder_num = list()
@@ -222,63 +227,67 @@ folder_avg_area_ratio = list()
 for folder_path in folder_list:
     img_object_Bin = process_folder(folder_path, 'Bin')
     (Ls_bin, Ws_bin, cnt_area_bin, aspr_bin, ar_bin) = process_img_obj(img_object_Bin)
+    print('Binary completed.')
 
     img_object_Otsu = process_folder(folder_path, 'Otsu')
     (Ls_otsu, Ws_otsu, cnt_area_otsu, aspr_otsu, ar_otsu) = process_img_obj(img_object_Otsu)
+    print('Otsu completed.')
 
     img_object_Canny = process_folder(folder_path, 'Canny')
     (Ls_canny, Ws_canny, cnt_area_canny, aspr_canny, ar_canny) = process_img_obj(img_object_Canny)
+    print('Canny completed.')
 
     img_object_Adapt = process_folder(folder_path, 'Adaptive')
     (Ls_adapt, Ws_adapt, cnt_area_adapt, aspr_adapt, ar_adapt) = process_img_obj(img_object_Adapt)
+    print('Adaptive completed.')
 
     pickle.dump(((Ls_bin, Ws_bin, cnt_area_bin, aspr_bin, ar_bin), (Ls_otsu, Ws_otsu, cnt_area_otsu, aspr_otsu, ar_otsu),
                 (Ls_canny, Ws_canny, cnt_area_canny, aspr_canny, ar_canny), (Ls_adapt, Ws_adapt, cnt_area_adapt, aspr_adapt, ar_adapt)),
                 open(folder_path[-6:], "wb"))
 
-    plt.figure(folder_count)
-
-    plt.subplot(2, 3, 1)
-    plt.hist([cnt_area_bin, cnt_area_canny, cnt_area_otsu, cnt_area_adapt], histtype='bar', ec='black', label=['Binary', 'Canny', 'Otsu', 'Adaptive'], alpha=0.5)
-    # plt.hist(cnt_area_otsu, histtype='bar', ec='black', label='Otsu', alpha=0.5)
-    # plt.hist(cnt_area_bin, histtype='bar', ec='black', label='Binary', alpha=0.5)
-    # plt.hist(cnt_area_canny, histtype='bar', ec='black', label='Canny', alpha=0.5)
-    plt.title('Contour Area')
-    plt.legend(loc='upper right')
-
-    plt.subplot(2, 3, 2)
-    plt.hist([Ls_bin, Ls_canny, Ls_otsu, Ls_adapt], histtype='bar', ec='black', label=['Binary', 'Canny', 'Otsu', 'Adaptive'], alpha=0.5)
-    # plt.hist(Ls_otsu, histtype='bar', ec='black', label='Otsu', alpha=0.5)
-    # plt.hist(Ls_bin, histtype='bar', ec='black', label='Binary', alpha=0.5)
-    # plt.hist(Ls_canny, histtype='bar', ec='black', label='Canny', alpha=0.5)
-    plt.title('Major Axis')
-    plt.legend(loc='upper right')
-
-    plt.subplot(2, 3, 3)
-    plt.hist([Ws_bin, Ws_canny, Ws_otsu, Ws_adapt], histtype='bar', ec='black', label=['Binary', 'Canny', 'Otsu', 'Adaptive'], alpha=0.5)
-    # plt.hist(Ws_otsu, histtype='bar', ec='black', label='Otsu', alpha=0.5)
-    # plt.hist(Ws_bin, histtype='bar', ec='black', label='Binary', alpha=0.5)
-    # plt.hist(Ws_canny, histtype='bar', ec='black', label='Canny', alpha=0.5)
-    plt.title('Minor Axis')
-    plt.legend(loc='upper right')
-
-    plt.subplot(2, 3, 4)
-    plt.hist([aspr_bin, aspr_canny, aspr_otsu, aspr_adapt], bins=[1, 1.1, 1.2, 1.3, 1.4, 1.5, 2, 4], histtype='bar', ec='black', label=['Binary', 'Canny', 'Otsu', 'Adaptive'], alpha=0.5)
-    # plt.hist(Ls_otsu/Ws_otsu, histtype='bar', ec='black', label='Otsu', alpha=0.5)
-    # plt.hist(Ls_bin/Ws_bin, histtype='bar', ec='black', label='Binary', alpha=0.5)
-    # plt.hist(Ls_canny/Ws_canny, histtype='bar', ec='black', label='Canny', alpha=0.5)
-    plt.title('Aspect Ratio')
-    plt.legend(loc='upper right')
-
-    plt.subplot(2, 3, 5)
-    plt.hist([ar_bin, ar_canny, ar_otsu, ar_adapt], ec='black', label=['Binary', 'Canny', 'Otsu', 'Adaptive'], alpha=0.5)
-    # plt.hist(cnt_area_otsu/(Ls_otsu*Ws_otsu), ec='black', label='Otsu', alpha=0.5)
-    # plt.hist(cnt_area_bin/(Ls_bin*Ws_bin), ec='black', label='Binary', alpha=0.5)
-    # plt.hist(cnt_area_canny/(Ls_canny*Ws_canny), ec='black', label='Canny', alpha=0.5)
-    plt.title('Normalized Area')
-    plt.legend(loc='upper right')
-
-    plt.suptitle(folder_path)
+    # plt.figure(folder_count)
+    #
+    # plt.subplot(2, 3, 1)
+    # plt.hist([cnt_area_bin, cnt_area_canny, cnt_area_otsu, cnt_area_adapt], histtype='bar', ec='black', label=['Binary', 'Canny', 'Otsu', 'Adaptive'], alpha=0.5)
+    # # plt.hist(cnt_area_otsu, histtype='bar', ec='black', label='Otsu', alpha=0.5)
+    # # plt.hist(cnt_area_bin, histtype='bar', ec='black', label='Binary', alpha=0.5)
+    # # plt.hist(cnt_area_canny, histtype='bar', ec='black', label='Canny', alpha=0.5)
+    # plt.title('Contour Area')
+    # plt.legend(loc='upper right')
+    #
+    # plt.subplot(2, 3, 2)
+    # plt.hist([Ls_bin, Ls_canny, Ls_otsu, Ls_adapt], histtype='bar', ec='black', label=['Binary', 'Canny', 'Otsu', 'Adaptive'], alpha=0.5)
+    # # plt.hist(Ls_otsu, histtype='bar', ec='black', label='Otsu', alpha=0.5)
+    # # plt.hist(Ls_bin, histtype='bar', ec='black', label='Binary', alpha=0.5)
+    # # plt.hist(Ls_canny, histtype='bar', ec='black', label='Canny', alpha=0.5)
+    # plt.title('Major Axis')
+    # plt.legend(loc='upper right')
+    #
+    # plt.subplot(2, 3, 3)
+    # plt.hist([Ws_bin, Ws_canny, Ws_otsu, Ws_adapt], histtype='bar', ec='black', label=['Binary', 'Canny', 'Otsu', 'Adaptive'], alpha=0.5)
+    # # plt.hist(Ws_otsu, histtype='bar', ec='black', label='Otsu', alpha=0.5)
+    # # plt.hist(Ws_bin, histtype='bar', ec='black', label='Binary', alpha=0.5)
+    # # plt.hist(Ws_canny, histtype='bar', ec='black', label='Canny', alpha=0.5)
+    # plt.title('Minor Axis')
+    # plt.legend(loc='upper right')
+    #
+    # plt.subplot(2, 3, 4)
+    # plt.hist([aspr_bin, aspr_canny, aspr_otsu, aspr_adapt], bins=[1, 1.1, 1.2, 1.3, 1.4, 1.5, 2, 4], histtype='bar', ec='black', label=['Binary', 'Canny', 'Otsu', 'Adaptive'], alpha=0.5)
+    # # plt.hist(Ls_otsu/Ws_otsu, histtype='bar', ec='black', label='Otsu', alpha=0.5)
+    # # plt.hist(Ls_bin/Ws_bin, histtype='bar', ec='black', label='Binary', alpha=0.5)
+    # # plt.hist(Ls_canny/Ws_canny, histtype='bar', ec='black', label='Canny', alpha=0.5)
+    # plt.title('Aspect Ratio')
+    # plt.legend(loc='upper right')
+    #
+    # plt.subplot(2, 3, 5)
+    # plt.hist([ar_bin, ar_canny, ar_otsu, ar_adapt], ec='black', label=['Binary', 'Canny', 'Otsu', 'Adaptive'], alpha=0.5)
+    # # plt.hist(cnt_area_otsu/(Ls_otsu*Ws_otsu), ec='black', label='Otsu', alpha=0.5)
+    # # plt.hist(cnt_area_bin/(Ls_bin*Ws_bin), ec='black', label='Binary', alpha=0.5)
+    # # plt.hist(cnt_area_canny/(Ls_canny*Ws_canny), ec='black', label='Canny', alpha=0.5)
+    # plt.title('Normalized Area')
+    # plt.legend(loc='upper right')
+    #
+    # plt.suptitle(folder_path)
 
     folder_avg_area.append((cnt_area_bin.mean(), cnt_area_canny.mean(), cnt_area_otsu.mean(), cnt_area_adapt.mean()))
     folder_avg_area_ratio.append((ar_bin.mean(), ar_canny.mean(), ar_otsu.mean(), ar_adapt.mean()))
@@ -287,56 +296,56 @@ for folder_path in folder_list:
     folder_num.append((len(Ls_bin), len(Ls_canny), len(Ls_otsu), len(Ls_adapt)))
 
     folder_count += 1
+#
+# figure = plt.figure(folder_count)
+#
+# labels_filter = ['Binary', 'Canny', 'Otsu', 'Adaptive']
+# xs = np.arange(0, len(labels_filter), 1)
+# folder_avg_area = list(map(list, zip(*folder_avg_area)))
+# folder_avg_area_ratio = list(map(list, zip(*folder_avg_area_ratio)))
+# folder_avg_aspr = list(map(list, zip(*folder_avg_aspr)))
+# folder_avg_Ws = list(map(list, zip(*folder_avg_Ws)))
+# folder_num = list(map(list, zip(*folder_num)))
 
-figure = plt.figure(folder_count)
-
-labels_filter = ['Binary', 'Canny', 'Otsu', 'Adaptive']
-xs = np.arange(0, len(labels_filter), 1)
-folder_avg_area = list(map(list, zip(*folder_avg_area)))
-folder_avg_area_ratio = list(map(list, zip(*folder_avg_area_ratio)))
-folder_avg_aspr = list(map(list, zip(*folder_avg_aspr)))
-folder_avg_Ws = list(map(list, zip(*folder_avg_Ws)))
-folder_num = list(map(list, zip(*folder_num)))
-
-
-subplot1 = figure.add_subplot(2, 3, 1)
-for nums, label_flt in zip(folder_num, labels_filter):
-    subplot1.plot(nums, 'o', label=label_flt)
-subplot1.set_xticks(xs)
-subplot1.set_xticklabels(folder_list)
-subplot1.set_title('Number of objects')
-plt.legend()
-
-subplot2 = figure.add_subplot(2, 3, 2)
-for avg_Ws, label_flt in zip(folder_avg_Ws, labels_filter):
-    subplot2.plot(avg_Ws, 'o', label=label_flt)
-subplot2.set_xticks(xs)
-subplot2.set_xticklabels(folder_list)
-subplot2.set_title('Average major axis')
-plt.legend()
-
-subplot3 = figure.add_subplot(2, 3, 3)
-for avg_area, label_flt in zip(folder_avg_area, labels_filter):
-    subplot3.plot(avg_area, 'o', label=label_flt)
-subplot3.set_xticks(xs)
-subplot3.set_xticklabels(folder_list)
-subplot3.set_title('Average area')
-plt.legend()
-
-subplot4 = figure.add_subplot(2, 3, 4)
-for avg_aspr, label_flt in zip(folder_avg_aspr, labels_filter):
-    subplot4.plot(avg_aspr, 'o', label=label_flt)
-subplot4.set_xticks(xs)
-subplot4.set_xticklabels(folder_list)
-subplot4.set_title('Average aspect ratio')
-plt.legend()
-
-subplot5 = figure.add_subplot(2, 3, 5)
-for avg_ar, label_flt in zip(folder_avg_area_ratio, labels_filter):
-    subplot5.plot(avg_ar, 'o', label=label_flt)
-subplot5.set_xticks(xs)
-subplot5.set_xticklabels(folder_list)
-subplot5.set_title('Average area ratio')
-plt.legend()
-
-plt.show()
+#
+# subplot1 = figure.add_subplot(2, 3, 1)
+# for nums, label_flt in zip(folder_num, labels_filter):
+#     subplot1.plot(nums, 'o', label=label_flt)
+# subplot1.set_xticks(xs)
+# subplot1.set_xticklabels(folder_list)
+# subplot1.set_title('Number of objects')
+# plt.legend()
+#
+# subplot2 = figure.add_subplot(2, 3, 2)
+# for avg_Ws, label_flt in zip(folder_avg_Ws, labels_filter):
+#     subplot2.plot(avg_Ws, 'o', label=label_flt)
+# subplot2.set_xticks(xs)
+# subplot2.set_xticklabels(folder_list)
+# subplot2.set_title('Average major axis')
+# plt.legend()
+#
+# subplot3 = figure.add_subplot(2, 3, 3)
+# for avg_area, label_flt in zip(folder_avg_area, labels_filter):
+#     subplot3.plot(avg_area, 'o', label=label_flt)
+# subplot3.set_xticks(xs)
+# subplot3.set_xticklabels(folder_list)
+# subplot3.set_title('Average area')
+# plt.legend()
+#
+# subplot4 = figure.add_subplot(2, 3, 4)
+# for avg_aspr, label_flt in zip(folder_avg_aspr, labels_filter):
+#     subplot4.plot(avg_aspr, 'o', label=label_flt)
+# subplot4.set_xticks(xs)
+# subplot4.set_xticklabels(folder_list)
+# subplot4.set_title('Average aspect ratio')
+# plt.legend()
+#
+# subplot5 = figure.add_subplot(2, 3, 5)
+# for avg_ar, label_flt in zip(folder_avg_area_ratio, labels_filter):
+#     subplot5.plot(avg_ar, 'o', label=label_flt)
+# subplot5.set_xticks(xs)
+# subplot5.set_xticklabels(folder_list)
+# subplot5.set_title('Average area ratio')
+# plt.legend()
+#
+# plt.show()
--- PlotOnly.py
+++ PlotOnly.py
@@ -3,19 +3,28 @@ import os
 import numpy as np
 import pickle
 
-full_folder = os.listdir('result_binaries')
+
+full_folder = sorted(os.listdir('result_binaries'))
 folder_list = list(filter(lambda c: (c[-3] != ".") & (c[0] != ".") & (c != "img"), full_folder))
+
 
 folder_count = 1
 folder_num = list()
 folder_avg_area = list()
 folder_mdn_area = list()
+folder_std_area = list()
+
 folder_avg_Ws = list()
 folder_mdn_Ws = list()
+folder_std_Ws = list()
+
 folder_avg_aspr = list()
 folder_mdn_aspr = list()
+folder_std_aspr = list()
+
 folder_avg_area_ratio = list()
 folder_mdn_area_ratio = list()
+folder_std_area_ratio = list()
 
 label_array = ['Canny', 'Otsu', 'Adaptive']
 
@@ -51,15 +60,23 @@ for folder_path in folder_list:
     #
     # plt.suptitle(folder_path)
 
+    folder_num.append((len(dims_bin[0]), len(dims_canny[0]), len(dims_otsu[0]), len(dims_adapt[0])))
+
     folder_avg_area.append((dims_bin[2].mean(), dims_canny[2].mean(), dims_otsu[2].mean(), dims_adapt[2].mean()))
     folder_mdn_area.append((np.median(dims_bin[2]), np.median(dims_canny[2]), np.median(dims_otsu[2]), np.median(dims_adapt[2])))
+    folder_std_area.append((np.std(dims_bin[2]), np.std(dims_canny[2]), np.std(dims_otsu[2]), np.std(dims_adapt[2])))
+
     folder_avg_area_ratio.append((dims_bin[4].mean(), dims_canny[4].mean(), dims_otsu[4].mean(), dims_adapt[4].mean()))
     folder_mdn_area_ratio.append((np.median(dims_bin[4]), np.median(dims_canny[4]), np.median(dims_otsu[4]), np.median(dims_adapt[4])))
+    folder_std_area_ratio.append((np.std(dims_bin[4]), np.std(dims_canny[4]), np.std(dims_otsu[4]), np.std(dims_adapt[4])))
+
     folder_avg_aspr.append((dims_bin[3].mean(), dims_canny[3].mean(), dims_otsu[3].mean(), dims_adapt[3].mean()))
     folder_mdn_aspr.append((np.median(dims_bin[3]), np.median(dims_canny[3]), np.median(dims_otsu[3]), np.median(dims_adapt[3])))
+    folder_std_aspr.append((np.std(dims_bin[3]), np.std(dims_canny[3]), np.std(dims_otsu[3]), np.std(dims_adapt[3])))
+
     folder_avg_Ws.append((dims_bin[1].mean(), dims_canny[1].mean(), dims_otsu[1].mean(), dims_adapt[1].mean()))
     folder_mdn_Ws.append((np.median(dims_bin[1]), np.median(dims_canny[1]), np.median(dims_otsu[1]), np.median(dims_adapt[1])))
-    folder_num.append((len(dims_bin[0]), len(dims_canny[0]), len(dims_otsu[0]), len(dims_adapt[0])))
+    folder_std_Ws.append((np.std(dims_bin[1]), np.std(dims_canny[1]), np.std(dims_otsu[1]), np.std(dims_adapt[1])))
 
     folder_count += 1
 
@@ -69,13 +86,22 @@ labels_filter = ['Binary', 'Canny', 'Otsu', 'Adaptive']
 x = np.arange(0, len(folder_list), 1)
 folder_avg_area = list(map(list, zip(*folder_avg_area)))
 folder_mdn_area = list(map(list, zip(*folder_mdn_area)))
+folder_std_area = list(map(list, zip(*folder_std_area)))
+
 folder_avg_area_ratio = list(map(list, zip(*folder_avg_area_ratio)))
 folder_mdn_area_ratio = list(map(list, zip(*folder_mdn_area_ratio)))
+folder_std_area_ratio = list(map(list, zip(*folder_std_area_ratio)))
+
 folder_avg_aspr = list(map(list, zip(*folder_avg_aspr)))
 folder_mdn_aspr = list(map(list, zip(*folder_mdn_aspr)))
+folder_std_aspr = list(map(list, zip(*folder_std_aspr)))
+
 folder_avg_Ws = list(map(list, zip(*folder_avg_Ws)))
 folder_mdn_Ws = list(map(list, zip(*folder_mdn_Ws)))
+folder_std_Ws = list(map(list, zip(*folder_std_Ws)))
+
 folder_num = list(map(list, zip(*folder_num)))
+
 
 cmap = plt.get_cmap('jet')
 col_mod = cmap(np.arange(1, len(labels_filter)+1)/len(labels_filter))
@@ -86,15 +112,19 @@ for col_idx, (nums, label_flt) in enumerate(zip(folder_num, labels_filter)):
     subplot1.plot(nums, 'o-', c=col_mod[col_idx],  label=label_flt)
 subplot1.set_xticks(x)
 subplot1.set_xticklabels(folder_list)
+subplot1.set_ylim([0, 350])
 subplot1.set_title('Number of objects')
 plt.legend()
 
 subplot2 = figure.add_subplot(2, 3, 2)
-for col_idx, (avg_Ws, mdn_Ws, label_flt) in enumerate(zip(folder_avg_Ws, folder_mdn_Ws, labels_filter)):
+for col_idx, (mdn_Ws, avg_Ws, label_flt) in enumerate(zip(folder_mdn_Ws, folder_avg_Ws, labels_filter)):
     subplot2.plot(avg_Ws, 'o-', c=col_mod[col_idx], label=label_flt)
     subplot2.plot(mdn_Ws, '*--', c=col_mod[col_idx])
+    #subplot2.plot(list(np.asarray(mdn_Ws)+np.asarray(std_Ws)), '*--', c=col_mod[col_idx])
+    #subplot2.plot(list(np.asarray(mdn_Ws)-np.asarray(std_Ws)), '*--', c=col_mod[col_idx])
 subplot2.set_xticks(x)
 subplot2.set_xticklabels(folder_list)
+subplot2.set_ylim([0, 55])
 subplot2.set_title('Average major axis')
 plt.legend()
 
@@ -104,6 +134,7 @@ for col_idx, (avg_area, mdn_area, label_flt) in enumerate(zip(folder_avg_area, f
     subplot3.plot(mdn_area, '*--', c=col_mod[col_idx])
 subplot3.set_xticks(x)
 subplot3.set_xticklabels(folder_list)
+subplot3.set_ylim([250, 3500])
 subplot3.set_title('Average area')
 plt.legend()
 
@@ -113,6 +144,7 @@ for col_idx, (avg_aspr, mdn_aspr, label_flt) in enumerate(zip(folder_avg_aspr, f
     subplot4.plot(mdn_aspr, '*--', c=col_mod[col_idx])
 subplot4.set_xticks(x)
 subplot4.set_xticklabels(folder_list)
+subplot4.set_ylim([1, 2.25])
 subplot4.set_title('Average aspect ratio')
 plt.legend()
 
@@ -122,6 +154,7 @@ for col_idx, (avg_ar, mdn_ar, label_flt) in enumerate(zip(folder_avg_area_ratio,
     subplot5.plot(mdn_ar, '*--', c=col_mod[col_idx])
 subplot5.set_xticks(x)
 subplot5.set_xticklabels(folder_list)
+subplot5.set_ylim([0.4, 0.8])
 subplot5.set_title('Average area ratio')
 plt.legend()
 
