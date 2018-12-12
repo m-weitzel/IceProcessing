4de09edb7cc8ea670d08eef46d1783e8a47704cb
Maximilian Weitzel <maweitze@uni-mainz.de>
2018-12-12 17:46:47 +0100

    Adjustment to default plotting properties and functionality

--- utilities/make_pretty_figure.py
+++ utilities/make_pretty_figure.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def imshow_in_figure(img=None, ax=None, xlabel=None, ylabel=None, title=None, hide_axes=False, figspan=(10, 10), dpi=100, grid=False):
+def imshow_in_figure(img=None, ax=None, xlabel=None, ylabel=None, title=None, hide_axes=False, figspan=(10, 10), dpi=100, grid=True):
 
     fig = plt.figure(figsize=figspan, dpi=dpi)
     if ax is None:
@@ -23,7 +23,8 @@ def imshow_in_figure(img=None, ax=None, xlabel=None, ylabel=None, title=None, hi
     ax.set_ylabel(ylabel, fontsize=20)
 
     ax.set_title(title, fontsize=28)
-    ax.grid(b=grid, which='major', linestyle='-')
+    if grid:
+        ax.grid(b=grid, which='major', linestyle='-')
 
     ax.tick_params(axis='both', which='major', labelsize=20)
 
@@ -34,10 +35,17 @@ def imshow_in_figure(img=None, ax=None, xlabel=None, ylabel=None, title=None, hi
     return fig, ax
 
 
-def create_hist(vals, minval, maxval, step=1, **kwargs):
+def create_hist(vals, minval=0, maxval=None, bins=None, step=1, **kwargs):
 
-    bins = np.arange(minval, maxval, step)
+    if maxval is None:
+        maxval = np.max(vals)
 
-    fig, ax = imshow_in_figure(grid=True, **kwargs)
-    ax.hist(vals, bins, edgecolor='black', linewidth=1.2)
+    if bins is None:
+        bins = np.arange(minval, maxval, step)
+
+    fig, ax = imshow_in_figure(grid=True, figspan=(10, 5), **kwargs)
+    ax.hist(vals, bins, edgecolor='black', linewidth=1.2, density=True)
+
+    return fig, ax
+
 
