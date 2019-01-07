0b8de6e64bb2024bec675ee3b56ff25f889e8f17
Maximilian Weitzel <maweitze@uni-mainz.de>
2019-01-07 15:12:39 +0100

    Added customization to standardized size distributions

--- utilities/plot_size_distribution.py
+++ utilities/plot_size_distribution.py
@@ -1,13 +1,23 @@
 import matplotlib.pyplot as plt
 
 
-def plot_size_dist(sizes, n_bins, normed=True, log=False):
+def plot_size_dist(sizes, n_bins, normed=True, log=False, lims=False, xlabel=''):
     fig_h = plt.figure()
     ax = fig_h.add_subplot(111)
 
-    ax.hist(sizes, n_bins, histtype='step', fill=False, label='N = {}'.format(len(sizes)), linewidth=3, density=normed, log=log)
-    ax.set_xlabel('Particle Diameter', fontsize=20)
-    ax.set_ylabel('Count', fontsize=20)
+    if lims:
+        ax.hist(sizes, n_bins, histtype='step', fill=True, label='N = {}'.format(len(sizes)), linewidth=3, density=normed, log=log, edgecolor='k', range=lims[0])
+        ax.set_xlim(lims[0])
+        try:
+            ax.set_ylim(lims[1])
+        except IndexError:
+            pass
+    else:
+        ax.hist(sizes, n_bins, histtype='step', fill=True, label='N = {}'.format(len(sizes)), linewidth=3, density=normed, log=log, edgecolor='k')
+
+    ax.set_xlabel(xlabel, fontsize=20)
+    ax.set_ylabel('Relative occurence', fontsize=20)
+    ax.set_ylabel('Relative occurence', fontsize=20)
     ax.grid()
 
     return fig_h, ax
