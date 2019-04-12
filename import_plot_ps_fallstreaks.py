05d7039b5642ba941cebe19a5b2c4185b34d13e7
Maximilian Weitzel <maweitze@uni-mainz.de>
2019-04-12 15:30:35 +0200

    Fixed several bugs implemented while making print-out plots. Returned to manual importing, deleting temporary pickles immediately.

--- import_plot_ps_fallstreaks.py
+++ import_plot_ps_fallstreaks.py
@@ -4,16 +4,16 @@ from matplotlib import ticker
 import matplotlib.gridspec as gridspec
 
 from scipy import stats
-from cycler import cycler
-from itertools import cycle, compress, chain
+from itertools import compress, chain
+print('Standard imports done.')
 
 from Speed.Holography.generate_fallstreaks import ParticleStreak, FallParticle, refine_streaks, get_folder_framerate, eta
 from utilities.fit_powerlaw import fit_powerlaw
 from utilities.tempsave_extracted_streaks import *
 from utilities.make_pretty_figure import *
+from mpl_toolkits.mplot3d import Axes3D
 
 # from utilities.plot_size_distribution import plot_size_dist
-# from mpl_toolkits.mplot3d import Axes3D
 # import numpy as np
 # import pickle
 # from matplotlib.widgets import CheckButtons
@@ -21,7 +21,9 @@ from utilities.make_pretty_figure import *
 # import os
 # from matplotlib import pyplot as plt
 
+
 def main():
+    print('Getting started...')
     # Loading data ############################
 
     folder_list = list()
@@ -67,15 +69,23 @@ def main():
     separate_by = 'habit'
     rho_o = 2500
 
-    list_of_folder_streak_lists = load_streaks(folder_list, 'ps_streaks_file.dat')
-
+    # list_of_folder_streak_lists = load_streaks(folder_list, 'ps_streaks_file.dat')
+    list_of_folder_streak_lists = list()
     list_of_folder_dim_lists = list()
     list_of_folder_v_lists = list()
     info_list = list()
     temp_by_folder = list()
 
+    angle_leniency_deg = 20
+    min_streak_length = 5
+
     for i, folder in enumerate(folder_list):
-        this_folders_streak_list = list_of_folder_streak_lists[i]
+        tmp = pickle.load(open(os.path.join(folder, 'streak_data.dat'), 'rb'))
+        this_folders_streak_list = tmp['streaks']
+        del tmp
+        this_folders_streak_list = refine_streaks(this_folders_streak_list, angle_leniency_deg)
+        this_folders_streak_list = [a for a in this_folders_streak_list if (len(a.particle_streak) == min_streak_length)]
+        # this_folders_streak_list = list_of_folder_streak_lists[i]
         this_folders_mean_angle = np.mean([s.mean_angle for s in this_folders_streak_list])
         this_folders_dim_list = list()
         this_folders_v_list = list()
@@ -91,6 +101,7 @@ def main():
             #     pass
 
         list_of_folder_dim_lists.append(this_folders_dim_list)
+        list_of_folder_streak_lists.append(this_folders_streak_list)
         list_of_folder_v_lists.append(this_folders_v_list)
         # try:
         #     temp_by_folder.append(tmp['temperature'])
@@ -116,9 +127,6 @@ def main():
     selector_index_dict = dict()
 
     print('Passing {} fall tracks to scatter routine.'.format(len(full_dim_median_list)))
-
-    # fig, ax = v_dim_scatter(selector_index_dict, full_dim_list, full_dim_median_list, full_v_median_list, full_v_list, different_separators, full_im_list,
-    #                         full_streakid_list, info_list, full_pos_list)
 
     # fig_r, ax_r = imshow_in_figure()
     dynamic_D = westbrook(full_dim_median_list, full_v_median_list)
@@ -176,7 +184,11 @@ def main():
     #
     # Plotting
     #
+    # different_separators.remove('CappedColumn')
+    # different_separators.remove('Needle')
+    # different_separators.remove('BulletRosette')
     # different_separators.remove('Unclassified')
+    # different_separators.append('Unclassified')
     # different_separators.remove('Needle')
     # different_separators = ['Unclassified']
     # different_separators = ['Aggregate']
@@ -209,8 +221,11 @@ def main():
     #         p3d(f_3d, p)
 
     print('Plotting scatter...')
-    fig, ax = v_dim_scatter(selector_index_dict, full_dim_list, full_dim_median_list, dynamic_D, full_v_list, different_separators, full_im_list, full_streakid_list, info_list, full_pos_list)
-    ax.plot(np.arange(200), np.arange(200), label='D=R', lw=2, c='b')
+
+    fig, ax = v_dim_scatter(selector_index_dict, full_dim_list, full_dim_median_list, full_v_median_list, full_v_list, different_separators, full_im_list, full_streakid_list, info_list, full_pos_list)
+    # fig, ax = v_dim_scatter(selector_index_dict, full_dim_list, full_dim_median_list, dynamic_D, full_v_list, different_separators, full_im_list, full_streakid_list, info_list, full_pos_list)
+
+    # ax.plot(np.arange(200), np.arange(200), label='D=R', lw=2, c='b')
 
     if calc_means:
         for sep in different_separators:
@@ -222,7 +237,7 @@ def main():
         plaw_vals_by_habits = dict()
         # colors = ['C1', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8']
         colors = ['tab:blue', 'tab:orange', 'tab:green', 'tab:red', 'tab:purple', 'tab:brown', 'tab:pink', 'tab:gray', 'tab:olive', 'tab:cyan']
-        for c, sep in zip(cycler(color=colors), different_separators):
+        for c, sep in zip(colors, different_separators):
             if len(dim_dict[sep]) > 5:
                 plaw_vals_by_habits[sep] = fit_powerlaw(dim_dict[sep], v_median_dict[sep])
                 powerlaw = lambda x, plaw_factor, plaw_exponent: plaw_factor * (x ** plaw_exponent)
@@ -231,7 +246,7 @@ def main():
                 # ax.plot(dim_dict[hab], plaw_by_habits[hab])
                 ax.plot(dim_dict[sep], plaw_by_habits[sep],
                         # label='{0}, v={1:.2f}d^{2:.2f}, R^2={3:.3f}'.format(sep, plaw_vals_by_habits[sep][0], plaw_vals_by_habits[sep][1], plaw_vals_by_habits[sep][2]))
-                        label=r'{0}, v={1:.2f}D^{2:.2f}'.format(sep, plaw_vals_by_habits[sep][0], plaw_vals_by_habits[sep][1]), linewidth=5, c=c['color'])
+                        label=r'{0}, v={1:.2f}D^{2:.2f}'.format(sep, plaw_vals_by_habits[sep][0], plaw_vals_by_habits[sep][1]), linewidth=5, c=c)
                 ax.legend(fontsize=8)
 
     if plot_stokes:
@@ -383,6 +398,7 @@ def load_streaks(folder_list, streak_fn, min_streak_length=3, angle_leniency_deg
         for folder in folder_list:
             tmp = pickle.load(open(os.path.join(folder, 'streak_data.dat'), 'rb'))
             tf_streak_list = tmp['streaks']
+            del tmp
             print('Added {} streaks from {}.'.format(len(tf_streak_list), folder))
             tf_streak_list = refine_streaks(tf_streak_list, angle_leniency_deg)
             # try:
@@ -489,14 +505,14 @@ def v_dim_scatter(selector_list, dim_list, dim_median_list, v_median_list,
     # locatelli_hobbs = 0.69*(dims_spaced*1e-3)**0.41*1e3
 
     almost_black = '#262626'
-    fig = plt.figure(figsize=(18, 10), dpi=100)
+    fig = plt.figure(figsize=(18, 12), dpi=150)
     fig.canvas.set_window_title('v-D scatter')
     ax = fig.add_subplot(111)
 
     marker_dict = dict()
-    marker_dict['Column'] = 's'
+    marker_dict['Column'] = 'x'
     marker_dict['Aggregate'] = (10, 1, 0)
-    marker_dict['Unclassified'] = 'v'
+    marker_dict['Unclassified'] = '+'
     marker_dict['Particle_round'] = 'o'
     marker_dict['Plate'] = (6, 0, 0)
     marker_dict['Needle'] = 'D'
@@ -518,18 +534,21 @@ def v_dim_scatter(selector_list, dim_list, dim_median_list, v_median_list,
     # different_separators = ['Needle']
 
     # colors = ['C1', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8']
-    colors = ['tab:orange', 'tab:blue', 'tab:green', 'tab:purple', 'tab:brown', 'tab:pink', 'tab:gray', 'tab:red', 'tab:olive', 'tab:cyan']
+    colors = ['tab:orange', 'tab:blue', 'tab:green', 'tab:cyan', 'tab:red', 'tab:pink', 'tab:gray', 'tab:red', 'tab:olive', 'tab:purple']
     # sepd_v_max = 0
 
-    # for i, (c, sep) in enumerate(zip(cycler(color=colors), different_separators)):
     for i, (c, sep) in enumerate(zip(colors, different_separators)):
         t_dim = list(compress(dim_median_list, selector_list[sep]))
         t_v = list(compress(v_median_list, selector_list[sep]))
         if len(t_v) > 0:
-            t_ln = ax.scatter(t_dim, t_v, alpha=1,
+            if sep=='Unclassified':
+                zord = 0
+            else:
+                zord = 2
+            t_ln = ax.scatter(t_dim, t_v,
                               # linewidth=1, zorder=0, picker=i, marker='+', c=c['color'])
                               # linewidth=1, zorder=0, picker=i, marker=marker_dict[sep], c=c['color'])
-                                linewidth=1, zorder=0, picker=i, marker=marker_dict[sep], c=c)
+                                linewidth=1, picker=i, marker=marker_dict[sep], c=c, zorder=zord)
             t_ln.set_label('{} (N={})'.format(sep, sum(selector_list[sep])))
             # t_ln.set_label('v={0:.2f}$\pm${1:.2f}$m/s$\n D={2:.2f}$\pm${3:.2f}$\mu m$'.format(np.mean(t_v), np.std(t_v), np.mean(t_dim), np.std(t_dim)))
             lines.append(t_ln)
@@ -540,19 +559,22 @@ def v_dim_scatter(selector_list, dim_list, dim_median_list, v_median_list,
     # ax.plot(dims_spaced[1:], powerlaw(dims_spaced, amp_full, index_full)[1:], label='Power Law Full', linewidth=3, zorder=1)
     # ax.plot(dims_spaced, f, linewidth=3, label='Linear Capacitance Fit, v=aC, a={}]'.format(p1))
     # ax.set_xlim([0, np.max(dims_spaced)])
-    # ax.set_xlim([20, 45])
-    ax.set_xlabel('Maximum dimension in µm', fontsize=20)
-    ax.set_ylim([0, 1.05 * sepd_v_max])
+    # ax.set_xlim([20, 200])
+    ax.set_xlabel('Maximum dimension in µm', fontsize=24)
+    # ax.set_ylim([0, 1.05 * sepd_v_max])
     # ax.set_ylim([0, 1.1*maximum_vel])
-    # ax.set_ylim([0, 115])
-    ax.set_ylabel('Fall speed in mm/s', fontsize=20)
-    plt.rc('font', size=20)
-    ax.tick_params(axis='both', which='major', labelsize=20)
+    # ax.set_ylim([5, 500])
+    ax.set_ylabel(r'Hydrodynamic diameter in µm', fontsize=24)
+    plt.rc('font', size=24)
+    ax.tick_params(axis='both', which='major', labelsize=24)
+    ax.tick_params(axis='both', which='mino r', labelsize=20)
     # ax.plot(dims_spaced, locatelli_hobbs, label='Locatelli+Hobbs 74', linewidth=2, color='b')
     # ax.axhline(maximum_vel, linewidth=2, color='k', label='Maximum measurable velocity')
     # ax.set_title('Fall speed vs. dimension for {}'.format(habit))
     # ax.set_title('Fall speed vs. dimension for Calibration Beads of $30\mu$m diameter', fontsize=24)
-    ax.legend(fontsize=20, loc='upper left')
+    ax.legend(fontsize=24, loc='upper left')
+    # ax.set_xscale('log')
+    # ax.set_yscale('log')
 
     # check = CheckButtons(ax, different_habits, [True]*len(different_habits))
     # check.on_clicked(func(different_habits, lines))
@@ -645,8 +667,8 @@ def onpick(event, dim_list, dim_median_list, im_list, streakid_list, info_list, 
         n = len(im_list[global_streakid])
         fig_fullshape = (2, n+4)
 
-        fig_i.suptitle('{} No. {} in folder {}, local index {}'.format(hab, dataind,
-                       info_list[streakid_list[hab][dataind]]['folder'][47:], info_list[streakid_list[hab][dataind]]['local_index']), fontsize=20)
+        # fig_i.suptitle('{} No. {} in folder {}, local index {}'.format(hab, dataind,
+        #                info_list[streakid_list[hab][dataind]]['folder'][47:], info_list[streakid_list[hab][dataind]]['local_index']), fontsize=20)
         # fig_i.suptitle('Particle No. {} in folder {}, local index {}'.format(dataind,
         #                info_list[streakid_list[hab][dataind]]['folder'][47:], info_list[streakid_list[hab][dataind]]['local_index']), fontsize=20)
 
@@ -654,7 +676,6 @@ def onpick(event, dim_list, dim_median_list, im_list, streakid_list, info_list, 
         imshapes = [im.shape for im in iml]
         im_maxsize = (np.max([b[0] for b in imshapes]), np.max([b[1] for b in imshapes]))
 
-        # gs1 = gridspec.GridSpec(2, n)
         gs1 = gridspec.GridSpec(n, 2)
 
         # ax_list = [fig_i.add_subplot(ss) for ss in list(gs1)[:-2]]
@@ -666,7 +687,7 @@ def onpick(event, dim_list, dim_median_list, im_list, streakid_list, info_list, 
             frame1 = plt.gca()
             frame1.axes.xaxis.set_ticklabels([])
             frame1.axes.yaxis.set_ticklabels([])
-            ax.set_ylabel(m, fontsize=20, rotation=0)
+            ax.set_ylabel(m+1, fontsize=20, rotation=0)
             ax.set_ylim(0, im_maxsize[0])
             ax.set_xlim(0, im_maxsize[1])
             plt.subplots_adjust(wspace=0.8)
@@ -676,9 +697,10 @@ def onpick(event, dim_list, dim_median_list, im_list, streakid_list, info_list, 
         # ax1 = plt.subplot2grid(fig_fullshape, (1, 0), colspan=n)
         fdl = dim_list[global_streakid]
         ax = plt.subplot(gs1[:n, 1])
-        ax.plot(fdl, c='b', lw=3)
+        xrange = list(np.arange(1, len(fdl)+1))
+        ax.plot(xrange, fdl, c='b', lw=3)
         ax.axhline(y=dim_median_list[global_streakid], ls='--', c='b')
-        ax.set_xlim(0, len(fdl)-1)
+        ax.set_xlim(1, len(fdl))
         ax.set_ylim(0, 1.1*np.max(fdl))
         ax.xaxis.set_major_locator(ticker.MaxNLocator(integer=True))
         ax.set_xlabel('Index', fontsize=20)
@@ -686,7 +708,7 @@ def onpick(event, dim_list, dim_median_list, im_list, streakid_list, info_list, 
 
         ax = ax.twinx()
         fvl = full_v_list[global_streakid]
-        xrange = list(np.arange(1, len(fvl)+1)-0.5)
+        xrange = np.arange(1, len(fvl)+1)+0.5
         ax.plot(xrange, fvl, c='g', lw=3)
         ax.axhline(y=v_median_list[global_streakid], ls='--', c='g')
         ax.set_ylim(0, 2*np.max(fvl))
@@ -780,8 +802,9 @@ def plot_hists_by_habit(habit, streak_list, dim_median_list, aspr_list, info_lis
 
 
 def westbrook(dim_list, v_list):
-    parameterized_m_list = [0.03097*d**2.13 for d in dim_list]
-    R = [9.81/(6*np.pi*eta(-10))*m/(v*1000) if v > 20 else np.nan for m, v in zip(parameterized_m_list, v_list)]
+    # parameterized_m_list = [0.03097*(d*1e-6)**2.13 for d in dim_list]
+    parameterized_m_list = [0.00373*(d*1e-6)**1.91 for d in dim_list]  # Columns
+    R = [9.81/(6*np.pi*eta(-10))*m/(v*1e-3)*1e6 if v > 20 else np.nan for m, v in zip(parameterized_m_list, v_list)]
     D = [2*r for r in R]
     return D
 
